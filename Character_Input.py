ef04861a8a8030b3ffdf808e35e215a63f3a797b
George <14george@windowslive.com>
2017-10-19 04:47:55 +0300

    Update Character_Input.py

--- Character_Input.py
+++ Character_Input.py
@@ -1,4 +1,11 @@
+"""Create a program that asks the user to enter their name and their age. Print out a message addressed to them that tells them the 
+year that they will turn 100 years old.
 
+Extras:
+
+Add on to the previous program by asking the user for another number and printing out that many copies of the previous message. 
+Print out that many copies of the previous message on separate lines.
+"""
 
 #-------FUNCTIONS-------
 #Definition of the printMessage function which asks for a number for how many times to display a specific message.
